9f3a55c0450ea15161602be89d3fa3deedfb4fcd
callenrosario <c.allen.rosario@gmail.com>
2013-02-27 23:34:49 -0800

    Initial commit.

---
+++ main.rb
@@ -0,0 +1,26 @@
+# Returns the digital sum of a natural number.
+def digital_sum(n)
+  n.to_s.split("").collect(&:to_i).inject(:+)
+end
+
+max = 99
+result = 0
+
+# Returns the maximum digital sum of all natural
+# numbers a^b where a,b < 100. 
+#
+# Start with the maximum power and then incrementally
+# divide. Skip to the next value of 'a' when the maximum
+# possible digital sum cannot reach the current maximum
+# digital sum anymore.
+max.downto(0).each do |a|
+  power = a**max
+  max.downto(0).each do |b|
+    break if result > power.to_s.length * 9
+    sum = digital_sum(power)
+    result = sum if result < sum 
+    power /= a
+  end
+end
+
+p result
